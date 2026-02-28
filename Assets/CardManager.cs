71501ed052e416a03669da17f98b58181910891b
Jan Krzyszkowski <jan.krzyszkowski2@gmail.com>
2026-02-28 01:13:53 +0100

    3423432423

---
+++ Assets/CardManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CardManager : MonoBehaviour
+{ 
+    public List<Sprite> cardImages;
+    public List<CardRarity> CardRarities = new List<CardRarity> { CardRarity.Common, CardRarity.Rare, CardRarity.Epic, CardRarity.Legendary };
+    public List<string> StatsToBoost = new List<string>(); 
+
+    public static CardManager instance;
+    
+    public void Awake()
+    {
+        instance = this;
+    }
+    
+   
+}
